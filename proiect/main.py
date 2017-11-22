80d492485f4083a94abe2671c6e0f33b285153f7
ptudorr <pop.tudorrr@gmail.com>
2017-11-22 07:25:37 -0600

    Added WebSockets and timer*

--- proiect/main.py
+++ proiect/main.py
@@ -21,22 +21,57 @@ def index():
 	return "Hello,  World!"
 
 
+@app.route('/ws')
+def index_ws():
+	return render_template('websocket.html')
 
-@socketio.on('my event', namespace='/test')
-def test_message(message):
-	emit('my response', {'data': message['data']})
+# @socketio.on('my event')
+# def test_message(message):
+#     emit('my response', {'data': 'got it!'})
+	
+@socketio.on('my event')
+def handle_my_custom_event(json):
+	print('received json: ' + str(json))
+	emit('my response', 'red', broadcast=True)
 
-@socketio.on('my broadcast event', namespace='/test')
-def test_message(message):
-	emit('my response', {'data': message['data']}, broadcast=True)
+lista_culori=['red','blue','yellow','white','green']
+counter=0
 
-@socketio.on('connect', namespace='/test')
-def test_connect():
-	emit('my response', {'data': 'Connected'})
+@socketio.on('ledul')
+def handle_my_custom_event(json):
+	print
+	print('received json: ' + str(json))
+	leddata={}
+	leddata['status']='ledoff'
+	leddata['culoare']='black'
+	global counter
+	leddata['numar']=counter
+	counter+=1
 
-@socketio.on('disconnect', namespace='/test')
-def test_disconnect():
-	print('Client  disconnected')
+
+	if unicode(str(json)).isnumeric():
+		leddata['status']='ledon'
+		leddata['culoare']=lista_culori[int(str(json))-1]
+
+
+	emit('schimba_culoarea',leddata, broadcast=True)
+
+
+# @socketio.on('my event', namespace='/test')
+# def test_message(message):
+# 	emit('my response', {'data': message['data']})
+
+# @socketio.on('my broadcast event', namespace='/test')
+# def test_message(message):
+# 	emit('my response', {'data': message['data']}, broadcast=True)
+
+# @socketio.on('connect', namespace='/test')
+# def test_connect():
+# 	emit('my response', {'data': 'Connected'})
+
+# @socketio.on('disconnect', namespace='/test')
+# def test_disconnect():
+# 	print('Client  disconnected')
 
 
 if __name__ == '__main__':
@@ -44,7 +79,6 @@ if __name__ == '__main__':
 	# t.daemon = True
 	# t.start()
 	# print 'pornim server HTTP'
-	app.run(host="0.0.0.0",port=server_port,debug=True)
-	# socketio.run(app,host="0.0.0.0",port=server_port,debug=True, use_reloader=False)
-
+	# app.run(host="0.0.0.0",port=server_port,debug=True)
+	socketio.run(app,host="0.0.0.0",port=server_port,debug=True, use_reloader=False)
 
--- proiect/static/validare.js
+++ proiect/static/validare.js
@@ -1,22 +1,14 @@
 function validare() {
-    var x = document.forms["form"]["FirstName"].value;
-    var y = document.forms["form"]["LastName"].value;
-    var z = document.forms["form"]["Mail"].value;
-    var t = document.forms["form"]["pwd"].value;
-    if (x == "" || y=="" || z=="" || t=="") {
-        alert("Completati va rag campurile libere!");
-        return false;
-    
+	var x = document.forms["form"]["FirstName"].value;
+	var y = document.forms["form"]["LastName"].value;
+	var z = document.forms["form"]["Mail"].value;
+	var t = document.forms["form"]["pwd"].value;
+	if (x == "" || y == "" || z == "" || t == "") {
+		alert("Completati va rag campurile libere!");
+		return false;
 
-}else{
-	document.getElementById("form").submit(); 
-}
-}
-//             // }else if (t!="000"){
-//     //     alert("Parola este gresita!");
-//     //     return false;
-//     // }else if(x != "" && y!="" && z!="" && t=="000"){
-//     //     window.location = "bineAiVenit.html";
-//     // }
 
-// } 
+	} else {
+		document.getElementById("form").submit();
+	}
+}
